b2de001f331064e34f453e969b3c627c6243d0cb
tylerwong <tjwong@dons.usfca.edu>
2021-01-28 14:42:38 -0800

    Adding rest of the 4 files
    Added index.html, p5.js, p5.sound.js, and sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+  textSize(40);
+  textAlign(CENTER);
+
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+  fill(240, 100, 0);
+  text("Hello, World", width/2, height/2);
+
+}
